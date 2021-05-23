2c6b7c540bdfd3b7836d062a80691e3f403b49c3
lequeux <maxence.lequeux.etu@univ-lille.fr>
2021-05-24 00:28:45 +0200

    step 02

--- Character.java
+++ Character.java
@@ -1,4 +1,4 @@
-public abstract class Character {
+public abstract class Character implements Movable {
 
     protected String RPGClass;
     protected String name;
@@ -7,7 +7,7 @@ public abstract class Character {
     protected int strength;
     protected int wit;
 
-    public Character(String p_name, String p_class){
+    public Character(String p_name, String p_class) {
         this.name = p_name;
         this.RPGClass = p_class;
         this.life = 50;
@@ -16,51 +16,67 @@ public abstract class Character {
         this.wit = 2;
     }
 
-    public String getRPGClass(){
+    public String getRPGClass() {
         return this.RPGClass;
     }
 
-    public String getName(){
+    public String getName() {
         return this.name;
     }
 
-    public int getLife(){
+    public int getLife() {
         return this.life;
     }
 
-    public int getAgility(){
+    public int getAgility() {
         return this.agility;
     }
 
-    public int getStrength(){
+    public int getStrength() {
         return this.strength;
     }
 
-    public int getWit(){
+    public int getWit() {
         return this.wit;
     }
 
-    public void setName(String p_name){
+    public void setName(String p_name) {
         this.name = p_name;
     }
 
-    public void setLife(int p_life){
+    public void setLife(int p_life) {
         this.life = p_life;
     }
 
-    public void setAgility(int p_agility){
+    public void setAgility(int p_agility) {
         this.agility = p_agility;
     }
 
-    public void setStrength(int p_strength){
+    public void setStrength(int p_strength) {
         this.strength = p_strength;
     }
 
-    public void setWit(int p_wit){
+    public void setWit(int p_wit) {
         this.wit = p_wit;
     }
 
-    public void attack(String weapon){
+    public void attack(String weapon) {
         System.out.println(this.name + ": Rrrrrrrrr....");
+    }
+
+    public void moveLeft(){
+        System.out.println(this.name + ": moves left");
+    }
+
+    public void moveRight(){
+        System.out.println(this.name + ": moves right");
+    }
+
+    public void moveForward(){
+        System.out.println(this.name + ": moves forward");
+    }
+
+    public void moveBack(){
+        System.out.println(this.name + ": moves back");
     }
 }
